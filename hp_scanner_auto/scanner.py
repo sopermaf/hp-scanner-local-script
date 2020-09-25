aa1ab290d4af883ffe24beec7aefe06bba3a182d
ferdia <ferdia@omko.co>
2020-09-25 14:01:58 +0100

    added remote debugging port

--- hp_scanner_auto/scanner.py
+++ hp_scanner_auto/scanner.py
@@ -49,6 +49,7 @@ def scan_and_save(printer_ip: str) -> None:
     options.add_argument("--disable-gpu")           # applicable to windows os only
     options.add_argument("--disable-dev-shm-usage") # overcome limited resource problems
     options.add_argument("--no-sandbox")
+    options.add_argument("--remote-debugging-port=9223")
     options.add_argument('--kiosk-printing')
 
     driver = webdriver.Chrome(options=options)
