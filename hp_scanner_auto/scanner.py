ef8a2e31c5936f7dfa87effc33de5e5ed96bb099
ferdia <ferdia@omko.co>
2020-09-25 14:41:55 +0100

    Added adf or glass option to scanner.py

--- hp_scanner_auto/scanner.py
+++ hp_scanner_auto/scanner.py
@@ -7,11 +7,16 @@ Only expected Documents to be PDF
 import json
 import os
 from argparse import ArgumentParser
+from enum import Enum
 from pathlib import Path
 from time import sleep
 
 from selenium import webdriver
 
+
+class FeedModes(Enum):
+    GLASS = '//*[@id="tmpId2-Inp"]/option[1]'
+    ADF = '//*[@id="tmpId2-Inp"]/option[2]'
 
 HOME_DIR = os.path.expanduser('~')
 DOWNLOADED_FILE = f"{HOME_DIR}/Downloads/NextDocument.pdf"
@@ -26,7 +31,7 @@ def automate_scan_process(printer_ip: str, filepath: str) -> None:
     mv_downloaded_scan(filepath)
 
 
-def scan_and_save(printer_ip: str) -> None:
+def scan_and_save(printer_ip: str, feed_mode: FeedModes=FeedModes.GLASS) -> None:
     """
     Saves as PDF in ~/Downloads/NextDocument.pdf
     """
@@ -55,8 +60,14 @@ def scan_and_save(printer_ip: str) -> None:
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(60)
 
-    # scan page
+    # connect to scanner
     driver.get(f"http://{printer_ip}#hId-pgWebScan")
+    
+
+    # select scan options and start
+    scan_feed = driver.find_element_by_xpath(feed_mode.value)
+    scan_feed.click()
+
     start_scan = driver.find_element_by_xpath('//*[@id="app-page-btns"]/div/input[1]')
     start_scan.click()
 
@@ -64,7 +75,6 @@ def scan_and_save(printer_ip: str) -> None:
     confirm.click()
 
     # switch focus to new tab
-    # driver.switch_to_window(driver.window_handles[1])
     driver.switch_to.window(driver.window_handles[1])
     sleep(20)
     driver.execute_script('window.print();')
@@ -93,13 +103,15 @@ if __name__ == "__main__":
     # setup argument parser
     parser = ArgumentParser()
     parser.add_argument('destination_fp', help="relative path to new file")
+    parser.add_argument('feed_mode', nargs='?', help='mode for feeding pages to scan')
     args = parser.parse_args()
 
     # create the scanned document
     printer_ip = os.environ['PRINTER_IP']
     print(f'\nPrinter IP: {printer_ip}')
 
-    scan_and_save(printer_ip)
+    feed_mode = args.feed_mode or FeedModes.ADF
+    scan_and_save(printer_ip, feed_mode)
     sleep(2)
 
     # move to the desired folder
